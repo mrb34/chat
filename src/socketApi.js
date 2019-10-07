977064eeabaeebd2066b83190fc1b7611f94a843
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-10-08 00:20:14 +0300

    socket authorization

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,9 +1,19 @@
 const socketio=require('socket.io');
+const socketAuthorization=require('../middlewares/socketAuthorization');
 const io=socketio();
 const socketApi={
     io
 };
+io.use(socketAuthorization);
+/*
+*redis adapter
+* */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter(
+    { host: process.env.REDIS_URI,
+        port: process.env.REDIS_PORT
+    }));
 io.on('connection',socket =>{
-console.log('a user logged in');
+console.log('a user logged in with name'+socket.request.user.surname);
 });
 module.exports=socketApi;
